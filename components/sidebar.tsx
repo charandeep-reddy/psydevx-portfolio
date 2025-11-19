0f680aa23ab3b835a3f7748f9f206c38ff712ace
Charandeep <sangaticherry3306@gmail.com>
2025-11-19 21:02:41 +0530

    feat: add font awesome social icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,11 +3,16 @@ import {
   CardTitle
 } from "@/components/ui/card";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import { Button } from "./ui/button";
 
 import { InfoCard } from "./infoCard";
 import { cn } from "@/lib/utils";
 import { infoConfig } from "./infoConfig";
+import Link from "next/link";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface SidebarProps {
     className?: string;
@@ -19,9 +24,32 @@ const sidebarConfig = {
   info: infoConfig
 }
 
+const socialConfig = [
+  {
+    title: "Facebook",
+    icon: faFacebook,
+    href: "https://www.facebook.com/example"
+  },
+  {
+    title: "Twitter",
+    icon: faTwitter,
+    href: "https://www.twitter.com/example"
+  },
+  {
+    title: "Instagram",
+    icon: faInstagram,
+    href: "https://www.instagram.com/example"
+  },
+  {
+    title: "LinkedIn",
+    icon: faLinkedin,
+    href: "https://www.linkedin.com/in/example"
+  },
+]
+
 export const Sidebar = ({ className }: SidebarProps) => {
   return (
-    <Card className={cn("flex flex-col gap-8 px-7.5 pb-7.5 py-15 rounded-2xl text-white h-fit", className)}>
+    <Card className={cn("flex flex-col gap-8 px-7.5 pb-7.5 pt-15 rounded-2xl text-white h-fit", className)}>
       <div className="flex flex-col gap-5 items-center justify-between">
         <div className="w-[150px] aspect-square rounded-3xl bg-white shadow-md"></div>
 
@@ -39,9 +67,11 @@ export const Sidebar = ({ className }: SidebarProps) => {
       </div>
 
       <div className="flex gap-4 items-center justify-center">
-        <div className="w-4 aspect-square rounded-full bg-white"></div>
-        <div className="w-4 aspect-square rounded-full bg-white"></div>
-        <div className="w-4 aspect-square rounded-full bg-white"></div>
+        {socialConfig.map((social: { title: string; href: string; icon: IconProp }) => (
+          <Link href={social.href} key={social.title} className="flex items-center gap-2">
+            <FontAwesomeIcon icon={social.icon} className="w-4" />
+          </Link>
+        ))}
       </div>
     </Card>
   );
