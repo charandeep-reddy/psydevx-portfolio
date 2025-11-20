317195353ce2925560a705912269a3bfc03a2357
Charandeep <sangaticherry3306@gmail.com>
2025-11-20 10:42:50 +0530

    fix: adjust bottom margin in PortfolioLayout component

--- components/layouts/portfolioLayout.tsx
+++ components/layouts/portfolioLayout.tsx
@@ -11,7 +11,7 @@ interface PortfolioLayoutProps {
 export function PortfolioLayout({ children, title }: PortfolioLayoutProps) {
 
   return (
-    <div className="lg:my-15 mt-5 mb-15 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-4 gap-7 w-full min-h-screen relative px-5 lg:px-0">
+    <div className="lg:my-15 mt-5 mb-20 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-4 gap-7 w-full min-h-screen relative px-5 lg:px-0">
       <Sidebar className="lg:col-span-1 lg:sticky lg:top-15" />
 
       <Card className="lg:col-span-3 min-h-screen p-7.5 relative w-full flex flex-col gap-10 rounded-[20px]">
