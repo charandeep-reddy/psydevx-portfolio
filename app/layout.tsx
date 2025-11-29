8c7dda9a779dea0e58353325cb9da67fe88dbf29
Charandeep <sangaticherry3306@gmail.com>
2025-11-29 09:57:03 +0530

    feat: add sonner notification component with rich colors prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "sonner"
 
 export const metadata: Metadata = {
   title: "Charan",
@@ -17,6 +18,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <Toaster richColors />
       </body>
     </html>
   );
