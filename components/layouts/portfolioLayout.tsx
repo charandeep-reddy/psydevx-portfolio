3cad9dbf632728d86a10625c7f70b495951d2d38
Charandeep <sangaticherry3306@gmail.com>
2025-11-29 09:27:17 +0530

    feat: add framer motion dependency and add fade in animation

--- components/layouts/portfolioLayout.tsx
+++ components/layouts/portfolioLayout.tsx
@@ -2,6 +2,8 @@
 import { Sidebar } from "@/components/sidebar";
 import { Card } from "@/components/ui/card";
 import { Navbar } from "@/components/navbar";
+import { motion } from "motion/react";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 interface PortfolioLayoutProps {
   children: React.ReactNode;
@@ -9,22 +11,28 @@ interface PortfolioLayoutProps {
 }
 
 export function PortfolioLayout({ children, title }: PortfolioLayoutProps) {
-
+  const isMobile = useIsMobile();
   return (
     <div className="lg:my-15 mt-5 mb-20 max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-4 gap-7 w-full min-h-screen relative px-5 lg:px-0">
       <Sidebar className="lg:col-span-1 lg:sticky lg:top-15" />
 
-      <Card className="lg:col-span-3 min-h-screen p-7.5 relative w-full flex flex-col gap-10 rounded-[20px]">
+      <Card className="lg:col-span-3 min-h-screen p-7.5 relative w-full rounded-[20px]">
         <Navbar />
-        {title && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="flex flex-col gap-10"
+        >
+          {title && (
             <div className="relative pb-7">
               <h1 className="text-4xl font-bold text-white">{title}</h1>
               <div className="absolute bottom-0 left-0 h-1.5 w-10 rounded-full bg-white"></div>
             </div>
-        )}
-        {children}
+          )}
+          {children}
+        </motion.div>
       </Card>
     </div>
   );
 }
-
